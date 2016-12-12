c56df90c69a1d582ef0fea190826b74fb192b94e
Andriy Batutin <andriy.batutin@email.com>
2016-12-12 12:36:16 +0200

    Initial Commit

---
+++ SilentRunner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SilentRunner
+//
+//  Created by andrew batutin on 12/12/16.
+//  Copyright © 2016 HomeOfRisingSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
