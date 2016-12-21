3f252a9242b46806da079cf3b15478673c2e44c1
Andriy Batutin <andriy.batutin@email.com>
2016-12-21 14:35:41 +0200

    added class for server

---
+++ SilentRunner/SilentRunnerEngine/server/SRServer.h
@@ -0,0 +1,15 @@
+//
+//  SRServer.h
+//  SilentRunner
+//
+//  Created by andrew batutin on 12/20/16.
+//  Copyright © 2016 HomeOfRisingSun. All rights reserved.
+//
+
+#import "SRConcreteArgument.h"
+
+@interface SRServer : SRConcreteArgument
+
+
+
+@end
---
+++ SilentRunner/SilentRunnerEngine/server/SRServer.m
@@ -0,0 +1,13 @@
+//
+//  SRServer.m
+//  SilentRunner
+//
+//  Created by andrew batutin on 12/20/16.
+//  Copyright © 2016 HomeOfRisingSun. All rights reserved.
+//
+
+#import "SRServer.h"
+
+@implementation SRServer
+
+@end
