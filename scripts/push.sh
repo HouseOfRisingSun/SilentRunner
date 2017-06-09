9d77b63337a5234e816d294cfb75a4f07df8f92e
Andrew Batutin <abatutin@gmail.com>
2017-06-09 13:36:28 +0300

    updated pod trunk script

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,4 +2,7 @@
 
 source ~/.rvm/scripts/rvm
 rvm use default
+#rm -rf ~/.cocoapods/repos/master
+pod setup
+pod lib lint --use-libraries --allow-warnings SilentRunnerEngine.podspec
 pod trunk push --use-libraries --allow-warnings
